f4c3e63b37acc26689865f34f3e28a331e86505c
Ansuman Mahapatra <ansumahaptra123@gmail.com>
2023-09-04 07:06:36 +0530

    Update about.js

--- src/pages/about.js
+++ src/pages/about.js
@@ -14,7 +14,7 @@ export default function About() {
         <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Qui provident consequuntur vel omnis quisquam rem harum, maxime expedita, ullam ut dolore! Distinctio eos minima voluptatum totam id hic! Sapiente debitis quia illum officia obcaecati provident nulla odio molestiae suscipit quasi.</p>
         <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Qui provident consequuntur vel omnis quisquam rem harum, maxime expedita, ullam ut dolore! Distinctio eos minima voluptatum totam id hic! Sapiente debitis quia illum officia obcaecati provident nulla odio molestiae suscipit quasi.</p>
       
-      <button onClick={()=>setU(null)}>LogOut</button>
+     
       
       </div>
     )
